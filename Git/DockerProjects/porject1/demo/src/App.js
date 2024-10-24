588e770ce2040d6a96569532e193d1d540abd82b
sumit kumar <sumitkry0125@email.com>
2024-10-25 00:55:01 +0530

    Changed css

--- Git/DockerProjects/porject1/demo/src/App.js
+++ Git/DockerProjects/porject1/demo/src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hii My Name is sumit and i am learning docker
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hi, my name is Sumit Kumar</h1>
+        <p>And I am learning Docker!</p>
+        <button onClick={() => alert('Welcome to my learning journey!')}>
+          Click me!
+        </button>
       </header>
     </div>
   );
